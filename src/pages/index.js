f7d1f1fc89fefcdab8e8b93ffe18db912e1b33b5
fredPack <fred.paquereau69@gmail.com>
2021-02-01 10:17:31 +0100

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,77 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Doughnut } from '@reactchartjs/react-chart.js'
+import { Pie } from '@reactchartjs/react-chart.js'
+import { Line } from '@reactchartjs/react-chart.js'
+import { Radar } from '@reactchartjs/react-chart.js'
+import axios from 'axios'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 100,
+      },
+      mutations: {
+        // mutation options
+      },
+    },
+})
+
+
+function Doug() {
+  
+const   queryDataBourse = ()=>{
+    let url  = "https://api.blockchain.com/v3/exchange/tickers"
+    const res = axios.get(url)
+    .then(resp => resp.data)
+    return  res
+}
+const { isLoading, error, data, isFetching } = useQuery("bourseData",  queryDataBourse,  {
+    refetchAllOnWindowFocus: false,
+})
+
+if (isFetching) return "";
+if (isLoading) return "...Loading";
+if (error) return  "An error has occurred: " + error.message;
+
+const color = data.map((k, i)=>{
+                let rand = () => Math.floor(Math.random() * 255)
+                return( 'rgba('+[rand(),rand(),rand(), 0.9]+')' )
+            })
+const borderColor= data.map((k, i)=>{
+                let rand = () => Math.floor(Math.random() * 255)
+                return( 'rgba('+[100,0,rand(), 0.5]+')' )
+            })
+const labels =  data.map (k=>( k.symbol ))
+const num =  data.map (k=>( k.price_24h))
+const volume =  data.map (k=>( k.volume_24h))
+const trade =  data.map (k=>( k.last_trade_price))
+
+
+const  datasetPrice =    [{label: '#cours', data:  num,  backgroundColor: color, borderColor:borderColor}]
+const dataFinalPrice =    {labels: labels, datasets: datasetPrice}
+
+const  datasetVolume =    [{label: '#volume', data:  volume,  backgroundColor: color, borderColor:borderColor}]
+const dataFinalVolume =    {labels: labels, datasets: datasetVolume}
+
+const  datasetTrade =    [{label: '#trade', data:  trade,  backgroundColor: color, borderColor:borderColor}]
+const dataFinalTrade =    {labels: labels, datasets: datasetTrade}
+
+return(
+  <>
+   <Doughnut data={dataFinalPrice} />
+   <Pie data={dataFinalVolume} />
+   <Line data={dataFinalPrice}/>
+   <Radar data={dataFinalTrade}/>
+  </>
 )
+}
 
-export default IndexPage
+export default function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Doug/>
+      </QueryClientProvider>
+    );
+  }
